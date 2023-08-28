209c7b00f26b684fc7eb678b18049b8d1e5b76e7
Haroldo Carvalho <hcarvalh@linkedin.biz>
2023-08-28 19:00:27 -0300

    update

--- brain.py
+++ brain.py
@@ -8,24 +8,26 @@ exec_path = os.getcwd()
 prediction = ImageClassification()
 # SqueezeNet model also no longer exists, now the fastest is MobileNetV2
 prediction.setModelTypeAsMobileNetV2()
-prediction.setModelPath(os.path.join(exec_path, 'mobilenet_v2-b0353104.pth'))
+prediction.setModelPath(os.path.join(exec_path, "mobilenet_v2-b0353104.pth"))
 prediction.loadModel()
 
-predctions, probabilities = prediction.classifyImage(os.path.join(exec_path,'house.jpg'), result_count=5)
+predctions, probabilities = prediction.classifyImage(
+    os.path.join(exec_path, "house.jpg"), result_count=5
+)
 for eachPred, eachProb in zip(predctions, probabilities):
-    print(f'{eachPred} : {eachProb}')
+    print(f"{eachPred} : {eachProb}")
 
 # -------------
 # Old Version:
-from imageai.Prediction import ImagePrediction
-import os
-execution_path=os.getcwd()
+# from imageai.Prediction import ImagePrediction
+# import os
+# execution_path=os.getcwd()
 
-prediction = ImagePrediction()
-prediction.setModelTypeAsSqueezeNet()
-prediction.setModelPath(os.path.join(execution_path, "squeezenet_weights_tf_dim_ordering_tf_kernels.h5"))
-prediction.loadModel()
+# prediction = ImagePrediction()
+# prediction.setModelTypeAsSqueezeNet()
+# prediction.setModelPath(os.path.join(execution_path, "squeezenet_weights_tf_dim_ordering_tf_kernels.h5"))
+# prediction.loadModel()
 
-predictions, probabilities = prediction.predictImage(os.path.join(execution_path, "giraffe.jpg"), result_count=5 )
-for eachPrediction, eachProbability in zip(predictions, probabilities):
-    print(eachPrediction , " : " , eachProbability)
+# predictions, probabilities = prediction.predictImage(os.path.join(execution_path, "giraffe.jpg"), result_count=5 )
+# for eachPrediction, eachProbability in zip(predictions, probabilities):
+#     print(eachPrediction , " : " , eachProbability)
